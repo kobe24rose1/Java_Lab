44f726ab7357160a26d7387f6ae1e424d79768cd
SeanLee <helloneteng@gmail.com>
2020-11-18 17:37:53 +0900

    making test

---
+++ chap06/src/staticex/StudentTest1.java
@@ -0,0 +1,26 @@
+package staticex;
+
+public class StudentTest1 {
+
+	public static void main(String[] args) {
+
+		Student studentLee = new Student();
+		Student studentKim = new Student();
+
+		System.out.println(studentLee.studentId);
+		System.out.println(studentKim.studentId);
+		System.out.println();
+
+		System.out.println(studentLee.serialNum);
+		System.out.println(studentKim.serialNum);
+		System.out.println();
+
+		studentLee.serialNum++;
+
+		System.out.println(studentLee.serialNum);
+		System.out.println(studentKim.serialNum);
+		System.out.println();
+
+	}
+
+}
